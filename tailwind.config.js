14ffed7bd1cc1908123ef5632a2e1aadbf88dfb0
BECKER Benjamin <benji.beck26@gmail.com>
2023-08-21 21:01:02 +0200

    ViteJs + Tailwind + MUI (default) + ChartJS
    Installation de :
    
    ViteJS
    Tailwind
    MUI
    ChartJS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Inclure les fichiers JavaScript et JSX
+    "./index.html", // Inclure le fichier HTML principal
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
