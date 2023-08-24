bffb82ac623834becb432eaf908754ab50dadc57
Jerome Marel <jerome.marel@oclock.school>
2023-08-24 11:29:33 +0200

    correction à la mano

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import LandingPage from './pages/LandingPage';
 import LoginPage from './components/Auth/Login';
 import RegisterPage from './components/Auth/Register';
 import DashBoardPage from './pages/dashboard/Dashboard';
-import Header from './components/Menu/Menu'; 
+import Header from './components/Menu/Menu';
+
+
+
+const PageWithHeader = ({ children }) => (
+  <div>
+    <Header />
+    {children}
+  </div>
+);
 
 const App = () => {
   return (
@@ -13,16 +22,10 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
-        
-        <Route
-          path="/dashboard"
-          element={
-            <div>
-              <Header />
-              <DashBoardPage />
-            </div>
-          }
-        />
+        <Route path="/dashboard" element={<PageWithHeader><DashBoardPage /></PageWithHeader>} />
+
+
+         
       </Routes>
     </div>
   );
