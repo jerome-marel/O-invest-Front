88e93ce14de580b77c016156e7f0550a6ab82e61
Jerome Marel <jerome.marel@oclock.school>
2023-08-24 10:45:00 +0200

    camembert

--- src/components/Chart/ChartCamembertDash.tsx
+++ src/components/Chart/ChartCamembertDash.tsx
@@ -1,14 +1,19 @@
+import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
 
 ChartJS.register(ArcElement, Tooltip);
+
+const formatPercentage = (value, total) => {
+  const percentage = ((value / total) * 100).toFixed(2);
+  return `${percentage}%`;
+};
 
 const data = {
   labels: ['Portefeuille 1', 'Portefeuille 2', 'Portefeuille 3'],
   datasets: [
     {
       data: [60, 25, 15],
-      label: ' % ',
       backgroundColor: ['#FF5733', '#FFC300', '#36A2EB'],
       hoverBackgroundColor: ['#FF8140', '#FFD233', '#5AC6FA'],
       borderWidth: 4,
@@ -18,34 +23,29 @@ const data = {
 
 const options = {
   cutout: 70,
+  responsive: true,
   maintainAspectRatio: true,
   plugins: {
     datalabels: {
-      color: '#fff', // Couleur du texte à l'intérieur des segments
+      color: '#fff',
       formatter: (value, ctx) => {
         let sum = 0;
         let dataArr = ctx.chart.data.datasets[0].data;
         dataArr.map(data => {
           sum += data;
         });
-        let percentage = ((value * 100) / sum).toFixed(2) + '%';
-        return percentage;
+        return formatPercentage(value, sum);
       },
     },
   },
-  
- 
-
   animation: {
     animateRotate: true,
     animateScale: true,
   },
 };
 
+
 const ChartCamembert = () => {
-
-    
-
   return (
     <div className="w-21 h-21">
       <Doughnut data={data} options={options} />
