f5905a46316a1731fb0ff908ae6e4f6960e664a8
Jerome Marel <jerome.marel@oclock.school>
2023-08-31 17:44:38 +0200

    gestion de l'ajout asset

--- src/components/assets/AssetListModal.tsx
+++ src/components/assets/AssetListModal.tsx
@@ -17,7 +17,7 @@ import { utcToZonedTime, format } from 'date-fns-tz';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'; 
 
-const AssetListModal = ({ isOpen, assets, onClose, portfolioId }) => {
+const AssetListModal = ({ isOpen, assets, onClose, portfolioId, handleAddAsset }) => {
   const overlayStyle = {
     backdropFilter: 'blur(8px)',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
@@ -79,7 +79,7 @@ const AssetListModal = ({ isOpen, assets, onClose, portfolioId }) => {
     setQuantity('');
   };
 
-  const handleAddAsset = () => {
+  const handleSubmit = () => {
     if (!selectedAsset || !selectedDate || !quantity) {
       console.error("Veuillez remplir tous les champs avant d'ajouter l'actif.");
       return;
@@ -99,6 +99,9 @@ const AssetListModal = ({ isOpen, assets, onClose, portfolioId }) => {
     axiosInstance.post(`/api/portfolios/${portfolioId}/addasset`, newAsset)
       .then(response => {
         console.log("Actif ajouté avec succès", response.data);
+        
+
+        handleAddAsset(response.data.newPortfolioAsset);
         handleCloseAssetModal();
         onClose();
 
@@ -107,7 +110,6 @@ const AssetListModal = ({ isOpen, assets, onClose, portfolioId }) => {
         console.log("data newtransaction", response.data.newTransaction);
 
         Navigate(`/portfolio/${portfolioId}`);
-        fetchAssets();
       })
       .catch(error => {
         console.log("data.portfolioAsset",response.data.portfolioAsset )
@@ -248,7 +250,7 @@ const AssetListModal = ({ isOpen, assets, onClose, portfolioId }) => {
             />
             <Button
               onClick={() => {
-                handleAddAsset();
+                handleSubmit();
                 handleCloseAssetModal(); // Fermer la modal après l'ajout
               }}
               variant="contained"
