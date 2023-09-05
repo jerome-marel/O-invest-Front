18508bb8c3da5d6b747e29725ce620a9497d5b17
Jerome Marel <jerome.marel@oclock.school>
2023-09-05 10:01:55 +0200

    correction

--- src/components/Portfolio/Asset.tsx
+++ src/components/Portfolio/Asset.tsx
@@ -1,24 +1,16 @@
-
 import AddAssetButton from "../AddAssetButton";
-// import NumberDisplay from '../Number/NumberDisplay';
 
-const AssetList = ({userPortfolioAssets, portfolioId }) => {
-
-const Asset = ({ userPortfolioAssets,  averagePrices, portfolioId, handleAddAsset, portfolio }) => {
-console.log("asseeeetttt", portfolio)
-
+const Asset = ({ userPortfolioAssets,  averagePrices, portfolioId, handleAddAsset }) => {
   return (
-
-    <div className="bg-[#0c0e15] text-white p-4 rounded-2xl shadow-md">
-      {//<div className="bg-white p-4 rounded-2xl shadow">}
+    <div className="bg-white p-4 rounded-2xl shadow">
       <div className="flex justify-between"> 
-      <h3 className="text-lg text-white font-semibold mb-4">Actifs du portefeuille</h3>
+      <h3 className="text-lg text-black font-semibold mb-4">Actifs du portefeuille</h3>
       <p><AddAssetButton onModalClose={() => {}} portfolioId={portfolioId} handleAddAsset={handleAddAsset} /> </p>
       </div>
       
       <table className="w-full">
         <thead>
-          <tr className=" bg-[#0c0e15] text-white">
+          <tr className=" text-black bg-gray-100">
             <th className="px-4 py-2">Nom</th>
             <th className="px-4 py-2">Prix actuel</th>
             <th className="px-4 py-2">Quantité</th>
@@ -33,13 +25,6 @@ console.log("asseeeetttt", portfolio)
             const averagePrice = averagePrices[asset.symbol] || 0; 
             return (
               <tr key={asset.id}>
-
-                <td className="text-white px-4 py-2">{asset.name}</td>
-                <td className="text-white px-4 py-2">{asset.historicPrice.toFixed(2)}</td>
-                <td className="text-white px-4 py-2">{asset.remainingQuantity}</td>
-                <td className="text-white px-4 py-2">{averagePrice}</td>
-                <td className="text-white px-4 py-2">{(asset.historicPrice * asset.remainingQuantity).toFixed(2)}</td>
-                <td className="text-white px-4 py-2"></td>
                 <td className="text-black px-4 py-2">{asset.name}</td>
                 <td className="text-black px-4 py-2">{asset.historicPrice.toFixed(2)}</td>
                 <td className="text-black px-4 py-2">{asset.remainingQuantity}</td>
@@ -56,5 +41,3 @@ console.log("asseeeetttt", portfolio)
 };
 
 export default Asset;
-
-
